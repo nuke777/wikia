263cf11256f5480f2512e83d6b73b80b7174cb8b
citronous <alg.4chan@gmail.com>
2019-02-20 20:23:27 +0800

    template

---
+++ Ships/essex.js
@@ -0,0 +1,181 @@
+<table style="width:100%;text-align:center;word-break:break-word">
+	<tbody>
+		<tr>
+			<td colspan="5" style="background:#f9f593">
+				<b>
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">Essex class aircraft carrier No. 1 </font>
+					</font>
+				</b>
+				<b>
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">Essex </font>
+					</font>
+				</b>
+				<b>
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">USS Essex</font>
+					</font>
+				</b>
+			</td>
+		</tr>
+		<tr>
+			<td style="width:20%;text-align:center" rowspan="3">
+				<img alt="Essex head.jpg" src="http://p8.qhimg.com/dr/100__/t0127f44ef7c74cf8fd.jpg" width="100" height="100" data-file-name="埃塞克斯头像.jpg" data-file-width="100" data-file-height="100">
+				</td>
+				<td style="width:20%">
+					<b>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">Numbering</font>
+						</font>
+					</b>
+				</td>
+				<td style="width:20%">
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;">NO. </font>
+					</font>
+					<span id="PNN">
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">357</font>
+						</font>
+					</span>
+				</td>
+				<td style="width:20%;padding-left: 0px;padding-right: 0px;">
+					<b>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">Initial star rating</font>
+						</font>
+					</b>
+				</td>
+				<td style="width:20%">
+					<font style="vertical-align: inherit;">
+						<font style="vertical-align: inherit;"> ★★★☆☆☆
+</font>
+					</font>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<b>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">Types of</font>
+						</font>
+					</b>
+				</td>
+				<td style="padding-left: 0px;padding-right: 0px;">
+					<a href="/blhx/%E5%88%86%E7%B1%BB:%E8%88%AA%E6%AF%8D" title="Aircraft carrier" target="_blank">
+						<img alt="Aircraft carrier" src="http://p2.qhimg.com/dr/30__/t012321038050a2efca.png" width="30" height="19" data-file-name="ShipType-航母.png" data-file-width="50" data-file-height="31">
+						</a>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">Aircraft carrier
+</font>
+						</font>
+					</td>
+					<td>
+						<b>
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">rarity</font>
+							</font>
+						</b>
+					</td>
+					<td>
+						<font style="vertical-align: inherit;">
+							<font style="vertical-align: inherit;">Super rare
+</font>
+						</font>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<b>
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">camp</font>
+							</font>
+						</b>
+					</td>
+					<td style="padding-left: 0px;padding-right: 0px;background:#">
+						<a href="/blhx/%E5%88%86%E7%B1%BB:%E9%98%B5%E8%90%A5:%E7%99%BD%E9%B9%B0" title="White eagle" target="_blank">
+							<img alt="White eagle" src="http://p7.qhimg.com/dr/30__/t01bf67b03a5536250b.png" width="30" height="30" data-file-name="Camplogo_白鹰.png" data-file-width="500" data-file-height="500">
+							</a>
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">White eagle
+</font>
+							</font>
+						</td>
+						<td>
+							<b>
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">time consuming</font>
+								</font>
+							</b>
+						</td>
+						<td>
+							<a href="/blhx/%E5%BB%BA%E9%80%A0%E6%97%B6%E9%97%B4" title="Construction time" target="_blank">
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">04:25:00 (activity is closed)</font>
+								</font>
+							</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">Ordinary drop point</font>
+								</font>
+							</b>
+						</td>
+						<td colspan="4">
+							<div style="display:block;max-height:100px;overflow-y:auto">
+								<a href="/blhx/%E9%99%90%E6%97%B6%E5%BB%BA%E9%80%A0" title="Limited time construction" class="mw-redirect" target="_blank">
+									<font style="vertical-align: inherit;">
+										<font style="vertical-align: inherit;">Limited time construction</font>
+									</font>
+								</a>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">Activity drop point</font>
+								</font>
+							</b>
+						</td>
+						<td colspan="4">
+							<div style="display:block;max-height:100px;overflow-y:auto"></div>
+						</td>
+					</tr>
+					<tr>
+						<td style="padding-left: 0px;padding-right: 0px">
+							<b>
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">Nutritional value</font>
+								</font>
+							</b>
+						</td>
+						<td colspan="4">
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">Shelling 0 Lightning Strike 0 Aviation 68 Loading 9
+</font>
+							</font>
+						</td>
+					</tr>
+					<tr>
+						<td style="padding-left: 0px;padding-right: 0px">
+							<b>
+								<font style="vertical-align: inherit;">
+									<font style="vertical-align: inherit;">Decommissioning income</font>
+								</font>
+							</b>
+						</td>
+						<td colspan="4">
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">Material 9 Oil 3 Medal 10
+</font>
+							</font>
+						</td>
+					</tr>
+				</tbody>
+			</table>
